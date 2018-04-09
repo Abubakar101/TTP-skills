ea582d7568cc7b1aca033c08d0a8f0a290e6c6a4
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-04-09 14:51:35 -0400

    Create question_3.js

---
+++ question_3.js
@@ -0,0 +1,27 @@
+// Question 3 -- getClosingParen(sentence, openingParenIndex):
+
+// "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing."
+
+// Write a function that, given a sentence like the one above, along with the position of an opening parenthesis, finds the corresponding closing parenthesis.
+// Example: if the example string above is input with the number 10 (position of the first parenthesis), the output should be 79 (position of the last parenthesis).
+
+function getClosingParen(sentence, openingParenIndex) {
+	let openParanCount = 0;
+
+	for (let i = openingParenIndex, j = sentence.length; i < j; i++) {
+		if (sentence[i] == '(') {
+			openParanCount++;
+		} else if (sentence[i] == ')') {
+			openParanCount--;
+
+			if (openParanCount === 0) {
+				return i;
+			}
+		}
+	}
+}
+
+getClosingParen(
+	'Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.',
+	27
+);
